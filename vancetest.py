813c2e8db070b2764ecec54597a3eb51b8197797
Krobix <45689440+Krobix@users.noreply.github.com>
2019-11-25 10:11:11 -0500

    Add files via upload

---
+++ vancetest.py
@@ -0,0 +1,14 @@
+import vance
+
+data = {
+	"lol": 0,
+	"lmao": "this is rad test",
+	"cry": ";-;"
+}
+
+obj = vance.createVanceData(data=data)
+with open("test.vance", "wb") as f:
+	vance.dump(obj, f)
+	
+
+print(vance.load(open("test.vance", "rb")))
