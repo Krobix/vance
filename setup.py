6fcbe75987427819e3c9ac94d0a533a885ed1176
Krobix <45689440+Krobix@users.noreply.github.com>
2019-11-25 09:49:29 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="vance",
+	author="Krobix",
+	author_email="brendon.shaw8921@gmail.com",
+	version="1.0b",
+	description="Simple data serialization file format",
+	packages=["vance"])
